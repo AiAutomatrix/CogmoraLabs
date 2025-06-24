b079984790c40e9fe3682b5662858632db334354
Bigtimetreat <Bigtimetreat00@gmail.com>
2025-06-24 16:25:11 +0000

    📈 Added Google Tag (gtag.js) to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip"; // Added TooltipProvider
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,8 +26,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark h-full">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0823M46Q58"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0823M46Q58');
+            `,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full flex flex-col`}>
-        <TooltipProvider> {/* Added TooltipProvider */}
+        <TooltipProvider>
           {children}
           <Toaster />
         </TooltipProvider>
