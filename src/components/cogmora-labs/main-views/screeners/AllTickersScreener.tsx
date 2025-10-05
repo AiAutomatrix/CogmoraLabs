f04bcbead4e37e2f3b74e4ee25e8d686cc817054
Garret Friesen <cogmoralabs@gmail.com>
2025-10-05 00:09:25 +0000

    the kucoin spot screener chart settings is messed up because it changes

--- src/components/cogmora-labs/main-views/screeners/AllTickersScreener.tsx
+++ src/components/cogmora-labs/main-views/screeners/AllTickersScreener.tsx
@@ -175,7 +175,7 @@ export default function AllTickersScreener({
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="sm"><Settings className="mr-2" /> Chart Setting: {selectedChartLayout}</Button>
+              <Button variant="outline" size="sm"><Settings className="mr-2 h-4 w-4" />{selectedChartLayout}</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               {chartOptions.map(num => (
@@ -297,3 +297,5 @@ export default function AllTickersScreener({
     </>
   );
 }
+
+    
