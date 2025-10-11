37e81b74d6fa7b64b9c04f8873e93869653aeb5a
Garret Friesen <cogmoralabs@gmail.com>
2025-10-11 18:14:25 +0000

    i reverted the repo back to our working production repo with the updates

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,7 @@ const nextConfig: NextConfig = {
       "http://6000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
       "https://9000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
       "http://9000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
+      "https://9000-firebase-studio-1759013558667.cluster-c72u3gwiofapkvxrcwjq5zllcu.cloudworkstations.dev",
     ],
   },
 };
