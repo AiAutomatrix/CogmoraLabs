9b1fc9a8c6f6f0efc6f5d666e7e02aa228fc625f
Garret Friesen <cogmoralabs@gmail.com>
2025-10-16 17:48:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2613744537-e60c7",
+  "appId": "1:1084135620241:web:8d9b766b81cf970c900930",
+  "apiKey": "AIzaSyCtkTPALNPLwJxBFuAjJlwWZmG9djJSfGc",
+  "authDomain": "studio-2613744537-e60c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1084135620241"
+};
