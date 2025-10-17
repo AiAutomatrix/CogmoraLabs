98687bbd4640920796e44eca82aa6f64c55eb0a8
Garret Friesen <cogmoralabs@gmail.com>
2025-10-17 23:46:07 +0000

    why can't we connect to our worker anymore. how do we make sure the same

--- usr/local/google/home/user/previews/studio-2613744537-e60c7/deploy.sh
+++ usr/local/google/home/user/previews/studio-2613744537-e60c7/deploy.sh
@@ -8,13 +8,6 @@ REGION="us-central1"
 SERVICE_NAME="realtime-worker"
 IMAGE="us-central1-docker.pkg.dev/$PROJECT_ID/docker-repo/$SERVICE_NAME:latest"
 SOURCE_DIR="realtime-worker"
-CLOUDBUILD_SA="1084135620241@cloudbuild.gserviceaccount.com"
-FULL_PROJECT_ID="studio-2613744537-e60c7"
-
-echo "🔑 Ensuring Cloud Build service account has Artifact Registry permissions..."
-gcloud projects add-iam-policy-binding $FULL_PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/artifactregistry.writer"
 
 echo "🔨 Building Docker image..."
 gcloud builds submit --tag $IMAGE $SOURCE_DIR
