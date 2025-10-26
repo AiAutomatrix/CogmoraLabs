2d175dd4dab230f7bd84beea0e66b3e58a4e2398
Garret Friesen <cogmoralabs@gmail.com>
2025-10-26 18:07:57 +0000

    you have to add these back in so i can see my app working in the preview

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
+      "http://6000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
+      "https://9000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
+      "http://9000-firebase-studio-1747864089303.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev",
+      "https://9000-firebase-studio-1759013558667.cluster-c72u3gwiofapkvxrcwjq5zllcu.cloudworkstations.dev",
+    ],
+  },
   images: {
     remotePatterns: [
       {
