4e9609fd99a550e74b5a0f49bed37aa3848ae8d8
Garret Friesen <cogmoralabs@gmail.com>
2025-11-04 18:30:26 +0000

    when a user clicks the trade button on the landing page watchlist is sho

--- src/components/landing/LandingPageWatchlistTradeTriggerPopup.tsx
+++ src/components/landing/LandingPageWatchlistTradeTriggerPopup.tsx
@@ -21,6 +21,7 @@ import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Separator } from '@/components/ui/separator';
+import { useToast } from '@/hooks/use-toast';
 
 interface LandingPageWatchlistTradeTriggerPopupProps {
   isOpen: boolean;
